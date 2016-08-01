7561948bc4d014df928b5c35d8cfa51ec7c72767
Keith Tomlinson <KBarrettTomlinson@gmail.com>
2016-08-01 18:27:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 var main=function()
 {
+$('.jumbotron').hide()
+$('home.jumbotron').show()
 $('p1').click(function()
   {
+  $('.jumbotron').hide();
+  $('Home.jumbotron').show();
+  }
+$('p2').click(function()
+  {
+  $('.jumbotron').hide();
+  $('About.jumbotron').show();
+  }
+$('p3').click(function()
+  {
+  $('.jumbotron').hide();
+  $('Education.jumbotron').show();
+  }
+$('p4').click(function()
+  {
+  $('jumbotron').hide();
+  $('EmploymentHistory.jumbotron').show();
+  $('EmploymentHistory.jumbotron').show();
+  $('EmploymentHistory.jumbotron').show();
+  $('EmploymentHistory.jumbotron').show();
+  $('EmploymentHistory.jumbotron').show();
+  }
 
 
 
