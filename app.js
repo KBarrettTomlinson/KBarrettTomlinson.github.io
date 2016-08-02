49f6cd81a1a192ef464e465d3f7857be5b3267a7
Keith Tomlinson <KBarrettTomlinson@gmail.com>
2016-08-01 21:23:25 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ $('.tab').click(function()
 $('.tab').removeClass('current');
 $(this).addClass('current');
 $('.jumbotrons').hide();
-document.getElementById("About.jumbotron").show();
+document.getElementById("About").show();
 }
 
 /*
