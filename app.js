fec04953381224379003f8f7bc111cd8fc750526
Keith Tomlinson <KBarrettTomlinson@gmail.com>
2016-08-07 17:01:01 -0500

    Update app.js

--- app.js
+++ app.js
@@ -20,55 +20,6 @@ $('.tab').on("click",function()
   $("#JEmploymentPeaceCoffee3.jumbotron").addClass('hide');
   }
   
-  /*
-  else if(id==="HEducation")
-    {
-    $('.tab').removeClass('current');
-    $(this).addClass('current');
-    $('.jumbotron').removeClass('hide');
-    $()
-    }
-    */
-}
-
-
-/*
-$('.tab').click(function()
-{
-$('.tab').removeClass('current');
-$(this).addClass('current');
-$('.jumbotron').removeClass('hide');
-}
-*/
-/*
-$('.jumbotron').hide()
-$('Home.jumbotron').show()
-$('.p1').click(function()
-  {
-  $('.jumbotron').hide();
-  $('Home.jumbotron').show();
-  }
-$('.p2').click(function()
-  {
-  $('.jumbotron').hide();
-  $('About.jumbotron').show();
-  }
-$('.p3').click(function()
-  {
-  $('.jumbotron').hide();
-  $('Education.jumbotron').show();
-  }
-$('.p4').click(function()
-  {
-  $('jumbotron').hide();
-  $('EmploymentHistory.jumbotron').show();
-  $('EmploymentPeaceCoffee1.jumbotron').show();
-  $('EmploymentPeaceCoffee2.jumbotron').show();
-  $('EmploymentBarista.jumbotron').show();
-  $('EmploymentPeaceCoffee3.jumbotron').show();
-  }
-*/
-
 ); 
 };
 $(document).ready(main);
